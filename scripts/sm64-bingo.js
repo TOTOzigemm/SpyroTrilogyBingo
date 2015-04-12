a061619939fe51ec0a8ec9d505d07f8eaa2a297d
TOTOzigemm <bowdishaw@yahoo.com>
2015-04-12 05:54:18 -0400

    Be committed

---
+++ scripts/sm64-bingo.js
@@ -0,0 +1,159 @@
+var bingoList = [];
+// Summer Forest 1
+bingoList[1] = [
+  { name: "Collect 75 gems in Dark Hollow", types: ["gems", "Dark Hollow"] },
+  { name: "Collect 250 gems in Dry Canyon", types: ["gems", "Dry Canyon"] },
+  { name: "Collect 225 gems in Doctor Shemp", types: ["gems", "Doctor Shemp"] },
+  { name: "Save 4 Dragons in Ice Cavern", types: ["dragons", "Ice Cavern"] }
+];
+bingoList[2] = [
+  { name: "Collect 175 gems in Idol Springs", types: ["gems", "Idol Springs"] },
+  { name: "Collect 225 gems in Glimmer", types: ["gems", "Glimmer"] },
+  { name: "Collect 200 gems in Colossus", types: ["gems", "Colossus"] },
+  { name: "Collect all Orbs in Hurricos", types: ["orbs", "Hurricos"] }
+];
+bingoList[3] = [
+  { name: "Collect 150 gems in Seashell Shore", types: ["gems", "Seashell Shore"] },
+  { name: "Collect all Eggs in Sunny Villa", types: ["eggs", "Sunny Villa"] },
+  { name: "Collect 225 gems in Enchanted Towers", types: ["gems", "Enchanted Towers"] },
+  { name: "Collect 250 gems in Bamboo Terrace", types: ["gems", "Bamboo Terrace"] }
+];
+bingoList[4] = [
+  { name: "100% all 'Sunny levels'", types: ["100%", "Sunny Flight", "Sunny Beach", "Sunny Villa"] },
+  { name: "100% all 'Hill' levels", types: ["100%", "Stone Hill", "Fracture Hills"] },
+  { name: "100% all 'Cloud' levels", types: ["100%", "Cloud Temples", "Cloud Spires"] },
+  { name: "Collect 10 Free Life bottles/chests", types: ["lives"]}
+];
+bingoList[5] = [
+  { name: "Stand on 10 Portals", types: ["silly"] },
+  { name: "Receive 3 Orbs from Hunter", types: ["orbs", "Hunter"] },
+  { name: "Collect all enemy spirits in 5 levels", types: ["enemies"] },
+  { name: "Collect 275 gems in Scorch", types: ["gems", "Scorch"] }
+];
+// 
+bingoList[6] = [
+  { name: "Save Noodles", types: ["silly", "Fireworks Factory"] },
+  { name: "Collect 10 eggs while swimming", types: ["eggs", "swim"] },
+  { name: "Collect 200 gems in Sgt. Byrd's Base", types: ["gems", "Sgt. Byrd's Base"] },
+  { name: "Collect all Eggs in Spooky Swamp", types: ["eggs", "Spooky Swamp"] }
+];
+bingoList[7] = [
+  { name: "100% all 'Town' levels", types: ["100%", "Town Square", "Cliff Town", "Spider Town"] },
+  { name: "100% all 'Canyon' levels", types: ["100%", "Dry Canyon", "Canyon Speedway"] },
+  { name: "100% all 'Ridge' levels", types: ["100%", "Alpine Ridge", "Charmed Ridge"] },
+  { name: "100% all 'Peak' levels", types: ["100%", "Wizard Peak", "Icy Peak"] }
+];
+bingoList[8] = [
+  { name: "100% all Peace Keepers levels including the homeworld", types: ["100%", "Peace Keepers", "Dry Canyon", "Cliff Town", "Ice Cavern", "Doctor Shemp", "Night Flight"] },
+  { name: "Collect 100 'non-enemy' gems in Ice Cavern", types: ["gems", "no-enemy", "Ice Cavern"] },
+  { name: "Collect 300 gems in Blowhard", types: ["gems", "Blowhard"] },
+  { name: "Save all 4 Dragons in Alpine Ridge", types: ["dragons", "Alpine Ridge"] }
+];
+bingoList[9] = [
+  { name: "Collect 15 orbs/eggs from mingames/side areas", types: ["orbs", "eggs"] },
+  { name: "Collect 100 gems in every Sunrise Springs level including the homeworld", types: ["gems", "Sunrise Springs", "Sunny Villa", "Cloud Spires", "Sheila's Alp", "Molten Crater", "Seashell Shore", "Mushroom Speedway"] },
+  { name: "Receive 4 Eggs from Hunter", types: ["eggs", "Hunter"] },
+  { name: "Collect 225 gems in Frozen Altars", types: ["gems", "Frozen Altars"] }
+];
+bingoList[10] = [
+  { name: "100% all 'Terrace' levels", types: ["100%", "Terrace Village", "Bamboo Terrace"] },
+  { name: "100% all 'Farm' levels", types: ["100%", "Robotica Farms", "Crawdad Farm"] },
+  { name: "Collect 7500 gems total", types: ["gems"] },
+  { name: "Complete 3 Mystery Jars", types: ["mystery jar"] }
+];
+// 
+bingoList[11] = [
+  { name: "Save all Spyro 1 eggs", types: ["Spyro 1", "Stone Hill", "Town Square", "Peace Keepers", "Dry Canyon", "Cliff Town", "Magic Crafters", "Alpine Ridge", "High Caves", "Wizard Peak"] },
+  { name: "Collect 200 'non-enemy' gems in 2 Beast Makers levels", types: ["gems", "non-enemy", "Beast Makers", "Terrace Village", "Misty Bog", "Tree Tops"] },
+  { name: "Save all 4 Dragons in Misty Bog", types: ["dragons", "Misty Bog"] },
+  { name: "Collect 400 gems in High Caves", types: ["gems", "High Caves"] }
+];
+bingoList[12] = [
+  { name: "Collect 20 orbs", types: ["orbs"] },
+  { name: "Collect 200 gems in every Summer Forest level including the homeworld", types: ["gems", "Summer Forest", "Glimmer", "Idol Springs", "Colossus", "Hurricos", "Sunny Beach", "Ocean Speedway", "Aquaria Towers"] },
+  { name: "Pay Money Bags 5 times", types: ["Money Bags"] },
+  { name: "Collect 225 gems in Zephyr", types: ["gems", "Zephyr"] }
+];
+bingoList[13] = [
+  { name: "100% all 'Icy' levels", types: ["100%", "Icy Flight", "Icy Speedway", "Icy Peak"] },
+  { name: "100% all 'Towers' levels", types: ["100%", "Haunted Towers", "Aquaria Towers", "Enchanted Towers"] },
+  { name: "Achieve 10 Skill Points", types: ["skill point"] },
+  { name: "Collect 5 Keys as Spyro", types: ["key"] }
+];
+// 
+bingoList[14] = [
+  { name: "100% all 'Crystal' levels", types: ["100%", "Crystal Flight", "Crystal Glacier", "Crystal Islands"] },
+  { name: "100% all 'Haunted' levels", types: ["100%", "Haunted Towers", "Haunted Tomb"] },
+  { name: "Collect 100000 gems", types: ["gems"] },
+  { name: "Collect 20 Free Life bottles/chests", types: ["lives"] }
+];
+bingoList[15] = [
+  { name: "100% all Beast Makers levels including the homeworld", types: ["100%", "Beast Makers", "Terrace Village", "Wild Flight", "Misty Bog", "Tree Tops", "Metalhead"] },
+  { name: "Get the Red Spring Chest", types: ["Lofty Castle"] },
+  { name: "Collect 300 gems in Lofty Castle", types: ["gems", "Lofty Castle"] },
+  { name: "Save all 3 Dragons in Dream Weavers", types: ["dragons", "Dream Weavers"] }
+];
+// 
+bingoList[16] = [
+  { name: "Receive 6 Orbs from Hunter", types: ["orbs", "Hunter"] },
+  { name: "Collect 250 gems in Shady Oasis", types: ["gems", "Shady Oasis"] },
+  { name: "Collect 250 gems in Metropolis", types: ["gems", "Metropolis"] },
+  { name: "Collect all Orbs in Mystic Marsh", types: ["orbs", "Mystic Marsh"] }
+];
+bingoList[17] = [
+  { name: "Collect 150 gems in every Midday Gardens level including the homeworld", types: ["gems", "Midday Gardens", "Icy Peak", "Enchanted Towers", "Spooky Swamp", "Sgt. Byrd's Base", "Bamboo Terrace", "Country Speedway"] },
+  { name: "Collect all Skateboarding Eggs", types: ["eggs", "Sunny Villa", "Enchanted Towers", "Lost Fleet", "Hunter"] },
+  { name: "Collect 250 gems in Charmed Ridge", types: ["gems", "Charmed Ridge"] },
+  { name: "Collect 275 gems in Fireworks Factory", types: ["gems", "Fireworks Factory"] }
+];
+bingoList[18] = [
+  { name: "100% all 'Dark' levels", types: ["100%", "Dark Hollow", "Dark Passage"] },
+  { name: "Collect 350 gems in Dark Passage", types: ["gems", "Dark Passage"] },
+  { name: "Collect 400 gems in Haunted Towers", types: ["gems", "Haunted Towers"] },
+  { name: "Save 4 Dragons in Dark Passage", types: ["dragons", "Dark Passage"] }
+];
+bingoList[19] = [
+  { name: "Collect 150 gems in every Autumn Plains level including the homeworld", types: ["gems", "Autumn Plains", "Crystal Glacier", "Skelos Badlands", "Zephyr", "Breeze Harbor", "Scorch", "Fracture Hills", "Magma Cone", "Shady Oasis", "Metro Speedway", "Icy Speedway"] },
+  { name: "Collect all enemy spirits in 10 levels", types: ["enemies"] },
+  { name: "Collect all Orbs in Fracture Hills", types: ["orbs", "Fracture Hills"] },
+  { name: "Collect 275 gems in Cloud Temples", types: ["gems", "Cloud Temples"] }
+];
+bingoList[20] = [
+  { name: "Collect 200 gems in every Evening Lake level including the homeworld", types: ["gems", "Evening Lake", "Frozen Altars", "Fireworks Factory", "Lost Fleet", "Charmed Ridge", "Bentley's Outpost", "Honey Speedway"] },
+  { name: "Receive 8 Eggs from Hunter", types: ["eggs", "Hunter"] },
+  { name: "Collect all Eggs in Lost Fleet", types: ["eggs", "Lost Fleet"] },
+  { name: "Collect 300 gems in Desert Ruins", types: ["gems", "Desert Ruins"] }
+];
+// 
+bingoList[21] = [
+  { name: "100% all 'Harbor' levels", types: ["100%", "Twilight Harbor", "Breeze Harbor", "Harbor Speedway"] },
+  { name: "Collect 12500 gems", types: ["gems"] },
+  { name: "Collect 30 Free Life bottles/chests", types: ["lives"] },
+  { name: "Complete ALL Mystery Jars", types: ["mystery jar"] }
+];
+bingoList[22] = [
+  { name: "Collect 40 Orbs", types: ["orbs"] },
+  { name: "Collect 100 gems in every Winter Tundra level including the homeworld", types: ["gems", "Winter Tundra", "Metropolis", "Robotica Farms", "Mystic Marsh", "Canyon Speedway", "Cloud Temple"] },
+  { name: "Collect all Homeworld Orbs", types: ["orbs", "Summer Forest", "Autumn Plains", "Winter Tundra"] },
+  { name: "Collect 30 orbs/eggs from minigames/side areas", types: ["orbs", "eggs"] }
+];
+bingoList[23] = [
+  { name: "Pay Money Bags 10 times", types: ["Money Bags"] },
+  { name: "Collect 250 gems in every Midnight Mountain level including the homeworld", types: ["gems", "Midnight Mountain", "Agent 9's Lab", "Crystal Island", "Desert Ruin", "Haunted Tomb", "Dino Mines", "Harbor Speedway"] },
+  { name: "Collect all Homeworld Eggs (except Money Bags)", types: ["eggs", "Sunrise Springs", "Midday Gardens", "Evening Lake", "Midnight Mountain"] },
+  { name: "Collect all Eggs in Dino Mines", types: ["eggs", "Dino Mines"] }
+];
+bingoList[24] = [
+  { name: "100% all 'Speedway' levels in Spyro 2", types: ["100%", "Ocean Speedway", "Metro Speedway", "Icy Speedway", "Canyon Speedway"] },
+  { name: "100% all 'Homeworld' levels", types: ["100%", "Artisans", "Peace Keepers", "Magic Crafters", "Beast Makers", "Dream Weavers", "Gnorc Nexus", "Summer Forest", "Autumn Plains", "Winter Tundra", "Sunrise Springs", "Midday Gardens", "Evening Lake", "Midnight Mountain"] },
+  { name: "Achieve 15 Skill Points", types: ["skill point"] },
+  { name: "Collect 10 Keys as Spyro", types: ["key"] }
+];
+bingoList[25] = [
+  { name: "Complete all Flights", types: ["100%", "Sunny Flight", "Night Flight", "Wild Flight", "Crystal Flight", "Icy Flight"] },
+  { name: "100% all Boss levels (Excluding Gnasty Gnorc)", types: ["100%", "Toasty", "Doctor Shemp", "Blowhard", "Metalhead", "Jacques"] },
+  { name: "Save Delbin Twice", types: ["dragons", "Artisans", "Gnorc Gnexus"] },
+  { name: "Use every super charge ramp in Spyro 1", types: ["Spyro 1", "Magic Crafters", "High Caves", "Wizard Peak", "Tree Tops", "Haunted Towers", "Lofty Castle", "Twilight Harbor"] }
+];
+
+$(function() { srl.bingo(bingoList, 5); });
